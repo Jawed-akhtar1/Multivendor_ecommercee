404d7df19dee1381b31f9242f3ed9cdc0c5c3a04
Mohd Jabir <157728756+Mohd-Jabir@users.noreply.github.com>
2026-08-13 17:24:23 +0530

    Update ProtectedRoute.jsx

--- multivendor-frontend/src/routes/ProtectedRoute.jsx
+++ multivendor-frontend/src/routes/ProtectedRoute.jsx
@@ -1,16 +1,11 @@
 import { Navigate, Outlet, useLocation } from "react-router-dom";
-
 import { useSelector } from "react-redux";
-
 const ProtectedRoute = () => {
   const { isAuthenticated } = useSelector((state) => state.auth);
-
   const location = useLocation();
-
   if (!isAuthenticated) {
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
-
   return <Outlet />;
 };
 
