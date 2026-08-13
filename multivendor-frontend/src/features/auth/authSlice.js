fe9766d50428c4ab91d6932c8eb565d570802f15
Mohd Jabir <157728756+Mohd-Jabir@users.noreply.github.com>
2026-08-13 17:31:25 +0530

    Update authSlice.js

--- multivendor-frontend/src/features/auth/authSlice.js
+++ multivendor-frontend/src/features/auth/authSlice.js
@@ -6,37 +6,29 @@ import {
   getUser,
   clearAuth,
 } from "../../services/authService";
-
 const initialState = {
   user: getUser(),
   token: getToken(),
   isAuthenticated: Boolean(getToken()),
 };
-
 const authSlice = createSlice({
   name: "auth",
-
   initialState,
-
   reducers: {
     setCredentials: (state, action) => {
       const user = saveAuth(action.payload);
-
       state.token = action.payload.token;
       state.user = user;
       state.isAuthenticated = true;
     },
-
     logout: (state) => {
       clearAuth();
-
       state.user = null;
       state.token = null;
       state.isAuthenticated = false;
     },
   },
 });
-
 export const {
   setCredentials,
   logout,
