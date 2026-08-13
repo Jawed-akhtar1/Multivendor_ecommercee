e405002fc4775552ff3c95b8288fdec707b77bdd
Mohd Jabir <157728756+Mohd-Jabir@users.noreply.github.com>
2026-08-13 17:24:40 +0530

    Update RoleRoute.jsx

--- multivendor-frontend/src/routes/RoleRoute.jsx
+++ multivendor-frontend/src/routes/RoleRoute.jsx
@@ -2,11 +2,9 @@ import { Outlet,Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 function RoleRoute({allowedRoles}){
     const { user, isAuthenticated } = useSelector((state) => state.auth);
-        
       if (!isAuthenticated) {
     return <Navigate to="/login" replace />;
   }
-
   if (!allowedRoles.includes(user?.role)) {
     return <Navigate to="/" replace />;
   }
