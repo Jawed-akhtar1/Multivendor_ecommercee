10dc3b41e56fd59e09e8d95bf484061665bfe9f5
Mohd-Jabir <ma7157229@gmail.com>
2026-08-15 16:19:57 +0530

    axios

--- multivendor-frontend/src/api/axios.js
+++ multivendor-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://multivendorecommercee-production.up.railway.app",
+  baseURL: "https://multivendorecommercee-production.up.railway.app/api",
   headers: {
     "Content-Type": "application/json",
   },
