9a96402195b52061f20b6d4671980c2e2590cd43
Mohd-Jabir <ma7157229@gmail.com>
2026-08-15 15:47:54 +0530

    fix railway deployment and api url

--- multivendor-frontend/src/api/axios.js
+++ multivendor-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://multivendorecommercee-production.up.railway.app",
   headers: {
     "Content-Type": "application/json",
   },
@@ -9,9 +9,11 @@ const api = axios.create({
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
