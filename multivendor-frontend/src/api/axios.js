49831fe473fc1c65f871266a886aa6a583a9f5a3
Mohd Jabir <157728756+Mohd-Jabir@users.noreply.github.com>
2026-08-15 19:25:49 +0530

    Refactor axios configuration for API requests

--- multivendor-frontend/src/api/axios.js
+++ multivendor-frontend/src/api/axios.js
@@ -1,19 +1,15 @@
 import axios from "axios";
-
 const api = axios.create({
   baseURL: "https://multivendorecommercee-production.up.railway.app/api",
   headers: {
     "Content-Type": "application/json",
   },
 });
-
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
-
   return config;
 });
 
