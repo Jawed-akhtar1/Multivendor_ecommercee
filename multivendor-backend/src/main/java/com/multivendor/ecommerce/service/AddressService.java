15a21cd2393ed02110770b484335c0999ef448f0
Mohd Jabir <157728756+Mohd-Jabir@users.noreply.github.com>
2026-08-24 12:25:42 +0530

    Refactor AddressService to use AddressResponse

--- multivendor-backend/src/main/java/com/multivendor/ecommerce/service/AddressService.java
+++ multivendor-backend/src/main/java/com/multivendor/ecommerce/service/AddressService.java
@@ -1,14 +1,22 @@
 package com.multivendor.ecommerce.service;
 
 import com.multivendor.ecommerce.dto.request.AddressRequest;
+import com.multivendor.ecommerce.dto.response.AddressResponse;
 import com.multivendor.ecommerce.entity.Address;
 
 import java.util.List;
 
 public interface AddressService {
-    Address add(Long userId, AddressRequest request);
-    Address update(Long userId, Long addressId, AddressRequest request);
+
+    AddressResponse add(Long userId, AddressRequest request);
+
+    AddressResponse update(Long userId, Long addressId, AddressRequest request);
+
     void delete(Long userId, Long addressId);
-    List<Address> getMyAddresses(Long userId);
+
+    /** Marks this address as the user's default, un-defaulting any other address they have. */
+    AddressResponse setDefault(Long userId, Long addressId);
+
+    List<AddressResponse> getMyAddresses(Long userId);
     Address getOwnedOrThrow(Long userId, Long addressId);
 }
